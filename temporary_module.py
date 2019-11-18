02bf43ade5045ed0376779ef0d7812d8894fe27f
kristiansantos11 <57390925+kristiansantos11@users.noreply.github.com>
2019-11-18 17:58:53 +0800

    Updated animation for temporary_module please test.
    TODO: White flash then main menu with DROP! logo

--- temporary_module.py
+++ temporary_module.py
@@ -9,14 +9,21 @@ from sprites import *
 
 pg.init()
 screen = pg.display.set_mode((500, 500))
+font_name = pg.font.match_font(font_name)
+clock = pg.time.Clock()
+clock.tick(fps)
+
+PULSE_EVENT = pg.USEREVENT + 1
 
 
-def start_screen_animation(width, height, color):
+def start_screen_animation(width, height, color, c_text):
     fade = pg.Surface((width, height))
     fade.fill((0, 0, 0))
+    draw_text(text[times], 20, white, 500 / 2, 500 / 2)
     for alpha in range(0, 255):
         fade.set_alpha(alpha)
         screen.fill((color))
+        draw_text(text[times], 20, c_text, 500 / 2, 500 / 2)
         screen.blit(fade, (0, 0))
         pg.display.flip()
         for event in pg.event.get():
@@ -25,26 +32,51 @@ def start_screen_animation(width, height, color):
             elif event.type == pg.KEYDOWN:
                 if event.key == pg.K_ESCAPE:
                     pg.quit()
-        pg.time.delay(1)
+
+
+def draw_text(text, size, color, x, y):
+    # function for drawing the text on the screen
+    font = pg.font.Font(font_name, size)
+    text_surface = font.render(text, True, color)
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x, y)
+    screen.blit(text_surface, text_rect)
 
 
 try:
     run = True
     while run:
+        show_main_menu = False
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 run = False
-            elif event.type == pg.KEYDOWN:
+            if event.type == pg.KEYDOWN:
                 if event.key == pg.K_ESCAPE:
                     run = False
                 if event.key == pg.K_RETURN:
-                    times = 0
-                    color = [(255, 255, 0), (255, 0, 255), (255, 0, 0), (125, 90, 100)]
-                    while times < 4:
-                        start_screen_animation(500, 500, color[random.randint(0, 3)])
-                        times += 1
-        clock = pg.time.Clock()
-        clock.tick(fps)
+                    pg.time.set_timer(PULSE_EVENT, 1)
+            if event.type == PULSE_EVENT:
+                times = 0
+                color = [(255, 255, 0), (255, 0, 255)]
+                text = ['Ready', 'Set']
+                color_text = [white, white, black]
+                while times < 2:
+                    start_screen_animation(500, 500, color[times], color_text[times])
+                    pg.display.flip()
+                    times += 1
+                pg.time.set_timer(PULSE_EVENT, 0)
+                show_main_menu = True
+        if show_main_menu:
+            new_screen = pg.Surface((500, 500))
+            new_screen.fill((255, 255, 255))
+            new_screen.set_alpha(255)
+            screen.blit(new_screen, (0, 0))
+            draw_text('DROP!', 50, black, 250, 250)
+            pg.display.flip()
+        else:
+            screen.fill((255, 255, 255))
+
+
 except pg.error:
     print("An error has occured within the program.")
 else:
